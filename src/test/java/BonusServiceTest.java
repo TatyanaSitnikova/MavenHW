08abda49e564454002eb43c642e8621c5f81ed68
TatyanaSitnikova <soulofsmm@gmail.com>
2023-08-25 20:00:26 +0300

    Fix #1

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,15 +7,15 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
+     
         long amount = 1000;
         boolean registered = true;
         long expected = 30;
 
-        // вызываем целевой метод:
+
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+
         Assertions.assertEquals(expected, actual);
     }
 
